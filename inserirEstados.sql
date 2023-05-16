86ed70649d8e27d91ed7a251e11c527c13031f94
nbeverton <nbeverton@gmail.com>
2023-05-16 15:52:57 -0300

    Inserindo estados
    Inserindo estados no exemplo.

---
+++ inserirEstados.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO estados
+    (nome, sigla, regiao, populacao)
+VALUES
+    ('Acre', 'AC', 'Norte', 0.83),
+    ('Alagoas', 'AL', 'Nordeste', 0.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
+    
